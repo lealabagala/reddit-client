2c905ed348202c0209bbe24f8f4249ac81557695
lealabagala <lea.labagala@gmail.com>
2020-01-11 09:19:14 +0800

    Setup skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
 import logo from './logo.svg';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <pre>{JSON.stringify(this.props)}</pre>
+        <button onClick={this.simpleAction}>Test redux action</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
